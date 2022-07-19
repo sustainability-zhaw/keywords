023fbc823fce155c548f4245d23d08a79524bcfc
Daniel Bajka <bajk@zhaw.ch>
2022-07-19 12:20:09 +0200

    Update dc_sdgs_mapping.R

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -16,9 +16,9 @@ function(workdir=wd) {
   return(workdir)
 }
 
-config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))#* @get /wd
+# config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))#* @get /wd
 
-#* @param cf
-function(cf = config) {
-  return(cf)
-}
+# #* @param cf
+# function(cf = config) {
+#   return(cf)
+# }
