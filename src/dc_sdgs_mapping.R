2b4d1200de709497a848f5d329a934d4698cad56
Daniel Bajka <bajk@zhaw.ch>
2022-07-19 11:33:18 +0200

    Update dc_sdgs_mapping.R

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -6,6 +6,12 @@ library(plumber)
 # repo <- "keywords/"
 
 # setwd(stringr::str_c(host, repo))
-wd = getwd()
+workdir = getwd()
 
-config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))
+#* @get /wd
+#* @param workdir
+function(workdir=wd) {
+  return(workdir)
+}
+
+# config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))
