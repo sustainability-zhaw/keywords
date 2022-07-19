9d620c664a8b162f77f940622b37be15102b877a
Daniel Bajka <bajk@zhaw.ch>
2022-07-19 14:21:40 +0200

    Update dc_sdgs_mapping.R

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -5,18 +5,23 @@ library(plumber)
 # host <- "/Users/bajk/documents/Github/sustainability/"
 # repo <- "keywords/"
 
-# setwd(stringr::str_c(host, repo))
-# setwd(stringr::str_c(getwd(), "../")
-# wd = here::here()
-wd = here::set_here(path='../')
-# wd = getwd()
+setwd('..')
+wdir = getwd()
 
 #* @get /wd
 #* @param workdir
-function(workdir=wd) {
+function(workdir=wdir) {
   # s = stringr::str_c("../", getwd(), "/config.yml")
   # s = stringr::str_c(workdir, "/config.yml")
   return(workdir)
 }
 
-# config <- yaml::read_yaml(stringr::str_c(wd, "../config.yml"))
+# config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))
+
+#* @get /conf
+#* @param wd
+function(wd=wdir) {
+  # s = stringr::str_c(wd, "/config.yml")
+  # s = yaml::read_yaml(stringr::str_c(wd, "/config.yml"))
+  return(wd)
+}
