d08030ae71abbd991772b4f9910ce10bf2ba92dc
Daniel Bajka <bajk@zhaw.ch>
2022-07-19 11:20:46 +0200

    Update dc_sdgs_mapping.R

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -1,4 +1,35 @@
 library(plumber)
-# library(qpcR)
 
 `%>%` = magrittr::`%>%` 
+
+host <- "/Users/bajk/documents/Github/sustainability/"
+repo <- "dc_test/"
+
+setwd(stringr::str_c(host, repo))
+wd = getwd()
+
+config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml"))
+
+# 
+#* @get /hello
+function() {
+  return("Hello World2")
+}
+
+
+#* @get /config
+function() {
+  return(config)
+}
+
+#* @get /wd
+#* @param workdir
+function(workdir = getwd()) {
+  return(workdir)
+}
+
+#* @get /wdir
+#* @param workdir
+function(workdir = wd) {
+  return(workdir)
+}
