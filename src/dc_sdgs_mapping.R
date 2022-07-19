f16f6fd3625a9abe732177fdc12a4d34c1a6d500
Daniel Bajka <bajk@zhaw.ch>
2022-07-19 09:25:27 +0200

    a

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -1,31 +1,6 @@
 library(plumber)
 # 
-# library(ghql)
-library(qpcR)
-
-`%>%` = magrittr::`%>%` 
-
-host <- "/Users/bajk/documents/Github/sustainability/"
-repo <- "keywords/"
-
-setwd(stringr::str_c(host, repo))
-wd = getwd()
-
-# wd <- ".."
-
-config <- yaml::read_yaml(stringr::str_c(wd, "/config.yml")) 
-
 #* @get /hello
 function() {
   return("Hello World")
-}
-
-#* @get /config
-function() {
-  return("config")
-}
-
-#* @get /wd
-function(wd = wd) {
-  return(wd)
 }
