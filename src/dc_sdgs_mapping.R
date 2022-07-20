eb5aed5741b17a63b2a3d40936c0686bbf6b197e
Daniel Bajka <bajk@zhaw.ch>
2022-07-20 09:04:50 +0200

    Update dc_sdgs_mapping.R

--- src/dc_sdgs_mapping.R
+++ src/dc_sdgs_mapping.R
@@ -126,10 +126,10 @@ function(dataIn = import_data(),
   ffiles_transformed = fconfig$pattern$files_transformed
   ffiles_sdg = fconfig$pattern$files_sdg
 
-# prepare the text to become the corpus
-text2analyse <-
-  dataIn$for_data_analysis[] %>%
-  stringr::str_replace_all("\\t", " ")
+  # prepare the text to become the corpus
+  text2analyse <-
+    dataIn$for_data_analysis[] %>%
+    stringr::str_replace_all("\\t", " ")
 
   # Basic corpus creation
   corp <- quanteda::corpus(text2analyse,
@@ -143,8 +143,369 @@ text2analyse <-
     dplyr::as_tibble() %>%
     dplyr::mutate(doc_id = as.numeric("0"))
 
+  
+  
+  # # Christians super simples beispiel um alle SDG Dateien von GH zu laden
+  # tidyr::tibble(
+  #   sdg = 1:16,
+  #   priorT = TRUE,
+  #   priorF = FALSE) %>%
+  #   tidyr::pivot_longer(startsWith("prior")) %>%
+  #   dplyr::group_by(sdg, name) %>%
+  #   dplyr::mutate(
+  #     data = import_sdgs_from_git(sdg, value) %>% list(),) %>%
+  #   dplyr::ungroup()
+  
+  # Main mapping routine
+  # sdgIn %>%
+  # purrr::map(., function(prior_posterior_list) {
+  # prior_posterior_list <- sdgIn # for debug
+  
+  prior_posterior_list <- sdgIn
+  
+  # Extract the sdg name
+  sdg_name <<-
+    prior_posterior_list$sdg_name
+  
+  # Extract the prior - posterior tibble
+  prior_posterior_tibble <-
+    prior_posterior_list$value %>%
+    dplyr::as_tibble() %>%
+    dplyr::select(1,2)
+  
+  # extract the prior vector
+  sdg_prior <-
+    prior_posterior_tibble$prior
+  
+  # extract the posterior vector
+  sdg_posterior <-
+    prior_posterior_tibble$posterior
+  
+  # detect all expression excluding sdgs
+  sdg_prior_NOT_priors <-
+    prior_posterior_tibble[sdg_prior %>%
+                             stringr::str_starts(pattern = "NOT ", negate = FALSE),]
+  
+  # detect all expression excluding sdgs
+  sdg_prior_NOT_prior <-
+    sdg_prior_NOT_priors %>%
+    dplyr::pull(prior) %>%
+    stringr::str_remove_all("^NOT") %>%
+    stringr::str_trim() %>%
+    as.list() %>%
+    unlist(., recursive = TRUE, use.names = TRUE) %>%
+    c("")
+  
+  # create a matching excluding n-gram for all keywords
+  prior_ngram_NOT <-
+    quanteda::kwic(corp,
+                   pattern = quanteda::phrase(sdg_prior_NOT_prior),
+                   separator = " ",
+                   case_insensitive = FALSE)
+  
+  
+  text_prior_by_not_reduced <-
+    dplyr::anti_join(quanteda::convert(corp, to = "data.frame"),
+                     prior_ngram_NOT,
+                     by = c("doc_id" = "docname"))
+  
+  # wranggled corpus creation
+  corp <-
+    quanteda::corpus(text_prior_by_not_reduced, meta = list())
+  
+  # split the corpus into sentenses
+  corp <-
+    quanteda::corpus_reshape(corp, to = "sentences", remove_punct = FALSE)
+  
+  
+  ###################### create a list containing all text blocks (sentenses) id's with text where the prior-posterior conditions states
+  # that this text block should be excluded.
+  
+  
+  # # detect all expression excluding sdgs
+  # sdg_prior_NOT_posterior <-
+  #   prior_posterior_tibble %>%
+  #     dplyr::mutate(
+  #       negate_post = posterior %>%
+  #         stringr::str_detect("^\\s*NOT ",
+  #                             negate = FALSE),
+  #       negate_post = ifelse(
+  #         is.na(negate_post),
+  #         FALSE,
+  #         negate_post
+  #       ),
+  #       posterior = posterior %>%
+  #         stringr::str_replace("^\\s*NOT ",
+  #                              "")
+  #     )
+  
+  # detect all expression excluding sdgs
+  sdg_prior_NOT_posterior <-
+    prior_posterior_tibble %>%
+    dplyr::mutate(
+      posterior = prior_posterior_tibble$posterior %>%
+        as.logical(.)
+    ) %>%
+    dplyr::filter(posterior != "")
+  
+  
+  sdg_prior_NOT_posterior_prior <-
+    sdg_prior_NOT_posterior %>%
+    dplyr::pull(prior) %>%
+    as.list() %>%
+    unlist(., recursive = TRUE, use.names = TRUE) #%>%
+  # c("")
+  
+  # detect all textblockes where the posterior starts with "NOT"
+  sdg_prior_NOT_posterior_posterior <-
+    sdg_prior_NOT_posterior %>%
+    dplyr::pull(posterior) %>%
+    stringr::str_remove_all("^NOT") %>%
+    stringr::str_trim() %>%
+    as.list() %>%
+    unlist(., recursive = TRUE, use.names = TRUE) #%>%
+  # c("")
+  
+  # name the created excluding sdg matrix
+  sdg_prior_NOT_posterior_tibble <-
+    dplyr::tibble(prior = sdg_prior_NOT_posterior_prior,
+                  posterior = sdg_prior_NOT_posterior_posterior)
+  
+  if (length(sdg_prior_NOT_posterior_prior) > 0 && all(!is.na(sdg_prior_NOT_posterior_prior))) {
+    # create a matching excluding n-gram for all keywords
+    prior_ngram_NOT <-
+      quanteda::kwic(corp,
+                     pattern = quanteda::phrase(sdg_prior_NOT_posterior_prior),
+                     separator = " ",
+                     case_insensitive = FALSE)
+    
+    # convert the found exccluding priors to a tibble
+    prior_ngram_NOT_tibble <-
+      prior_ngram_NOT %>%
+      dplyr::tibble() %>%
+      dplyr::select(doc_id = docname,
+                    prior = keyword) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(doc_id = doc_id %>%
+                      stringr::str_remove("\\.[0-9]+$") %>%
+                      stringr::str_remove("^[a-z]*") %>%
+                      as.numeric()
+      )
+  } else{
+    prior_ngram_NOT_tibble = dplyr::tibble(doc_id = "", prior = "")
+  }
+  # prior_ngram_NOT_tibble
+  
+  if (length(sdg_prior_NOT_posterior_posterior) > 0 && all(!is.na(sdg_prior_NOT_posterior_posterior))) {
+    # create a matching excluding n-gram for all keywords
+    posterior_ngram_exclude <-
+      quanteda::kwic(corp,
+                     pattern = quanteda::phrase(sdg_prior_NOT_posterior_posterior),
+                     separator = " ",
+                     case_insensitive = FALSE)
+    
+    # convert the found excluding priors to a tibble
+    posterior_ngram_exclude_tibble <-
+      posterior_ngram_exclude %>%
+      dplyr::tibble() %>%
+      dplyr::select(doc_id = docname,
+                    prior = keyword) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(doc_id = doc_id %>%
+                      stringr::str_remove("\\.[0-9]+$") %>%
+                      stringr::str_remove("^[a-z]*") %>%
+                      as.numeric()
+      )
+  } else{
+    posterior_ngram_exclude_tibble = dplyr::tibble(doc_id = "", prior = "")
+  }
+  
+  # Inner join the matching excluding priors and posteriors from the sdgs having a posterior
+  posterior_excluded_joined <-
+    dplyr::inner_join(prior_ngram_NOT_tibble,
+                      posterior_ngram_exclude_tibble,
+                      by = "doc_id") %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(doc_id = as.character(doc_id))
+  
+  # text reduction by all prior-posterior combinations to be excluded.
+  text_exclusion <-
+    dplyr::anti_join(quanteda::convert(corp,
+                                       to = "data.frame"),
+                     posterior_excluded_joined,
+                     by = "doc_id")
+  
+  # wranggled corpus creation
+  corp <-
+    quanteda::corpus(text_exclusion, meta = list())
+  
+  # split the corpus into sentenses
+  corp <-
+    quanteda::corpus_reshape(corp, to = "sentences", remove_punct = FALSE)
+  
+  
+  # create the prior list having no posterior
+  sdg_prior_no_posterior <-
+    prior_posterior_tibble %>%
+    dplyr::filter(posterior == "") %>%
+    # dplyr::add_row(prior = "a", posterior = "a") %>%
+    dplyr::pull(prior) %>%
+    as.list() %>%
+    unlist(recursive = TRUE, use.names = TRUE) %>%
+    c("")
+  
+  # create a matching n-gram for all keywords
+  prior_ngram <-
+    quanteda::kwic(corp,
+                   pattern = quanteda::phrase(sdg_prior_no_posterior),
+                   separator = " ",
+                   case_insensitive = FALSE)
+  
+  # convert the found priors to a tibble
+  prior_ngram_tibble <-
+    prior_ngram %>%
+    dplyr::tibble() %>%
+    dplyr::select(doc_id = docname,
+                  prior = keyword) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(doc_id = doc_id %>%
+                    stringr::str_remove("\\.[0-9]+$") %>%
+                    stringr::str_remove("^[a-z]*") %>%
+                    as.numeric(),
+                  # posterior_prior = NA %>% as.character()
+                  posterior_prior = NA
+    ) %>%
+    dplyr::distinct()
+  
+  sdg_prior_and_posterior <-
+    prior_posterior_tibble %>%
+    dplyr::filter(posterior != "")
+  
+  # sdg_prior_and_posterior
+  
+  # cummulated list of all priors
+  sdg_prior_and_posterior_prior <-
+    sdg_prior_and_posterior %>%
+    dplyr::pull(prior) %>%
+    as.list() %>%
+    unlist(recursive = TRUE, use.names = TRUE)
+  
+  # creating a tibble just containing the doc_id and the posteriors
+  sdg_prior_and_posterior_posterior <-
+    sdg_prior_and_posterior %>%
+    dplyr::pull(posterior) %>%
+    as.list() %>%
+    unlist(recursive = TRUE, use.names = TRUE)
+  
+  # sdg_prior_and_posterior_posterior
+  
+  
+  if (length(sdg_prior_and_posterior_posterior) > 0) {
+    
+    # create a matching n-gram for all conditions
+    posterior_prior_ngram <-
+      quanteda::kwic(corp,
+                     pattern = quanteda::phrase(sdg_prior_and_posterior_prior),
+                     separator = " ",
+                     case_insensitive = FALSE)
+    
+    
+    # convert the found posteriors to a tibble
+    posterior_prior_tibble <-
+      posterior_prior_ngram %>%
+      dplyr::tibble() %>%
+      dplyr::select(doc_id = docname, prior = keyword) %>%
+      dplyr::mutate(doc_id = doc_id %>%
+                      stringr::str_remove("\\.[0-9]+$") %>%
+                      stringr::str_remove("^[a-z]*") %>%
+                      as.numeric()
+      ) %>%
+      dplyr::distinct()
+    
+    
+    # create a matchin n-gram for all conditions
+    posterior_posterior_ngram <-
+      quanteda::kwic(corp,
+                     pattern = quanteda::phrase(sdg_prior_and_posterior_posterior),
+                     separator = " ",
+                     case_insensitive = FALSE)
+    # posterior_posterior_ngram
+    
+    # creating a list containing posteriors only
+    posterior_posterior_tibble <-
+      posterior_posterior_ngram %>%
+      dplyr::tibble() %>%
+      dplyr::select(doc_id = docname, posterior = keyword) %>%
+      dplyr::mutate(doc_id = doc_id %>%
+                      stringr::str_remove("\\.[0-9]+$") %>%
+                      stringr::str_remove("^[a-z]*") %>%
+                      as.numeric()
+      ) %>%
+      dplyr::distinct()
+    
+    
+    # Inner join the matching priors and posteriors from the sdgs having a posterior
+    posterior_joined <-
+      dplyr::inner_join(posterior_prior_tibble,
+                        posterior_posterior_tibble,
+                        by = "doc_id") %>%
+      dplyr::distinct()
+    
+    prior_posterior_joined <-
+      dplyr::full_join(prior_ngram_tibble %>% dplyr::as_tibble(),
+                       posterior_joined %>% dplyr::as_tibble(),
+                       by = c("doc_id", "prior")) %>%
+      dplyr::select("doc_id", "prior", "posterior")
+    
+    
+    # prior_posterior_joined[is.na(prior_posterior_joined)] <- 0
+    
+  } else {
+    prior_posterior_joined <-
+      prior_ngram_tibble %>%
+      dplyr::mutate(posterior = NA)
+  }
+  
+  # Convert the quanteda Corpus to a dataframe
+  corp_df <-
+    quanteda::convert(corp, to = "data.frame")
+  
+  # join all documents, splitted into sentences, to the found prior/posterior combinations of a specific sdg
+  prior_posterior_full_tibble <-
+    dplyr::tibble(corp_df)%>%
+    dplyr::mutate(doc_id = doc_id %>%
+                    stringr::str_remove("\\.[0-9]+$") %>%
+                    stringr::str_remove("^[a-z]*") %>%
+                    as.integer()
+    ) %>%
+    merge(., prior_posterior_joined,
+          by=c("doc_id")) %>%
+    dplyr::select("doc_id", "prior", "posterior") %>%
+    dplyr::distinct()
+  
+  # Prepare the sdg matching result for export
+  prior_posterior_match_tibble  <-
+    prior_posterior_full_tibble %>%
+    # dplyr::select(doc_id,
+    #               prior, posterior) %>%
+    dplyr::group_by(doc_id) %>%
+    dplyr::summarise(prior = prior, posterior,
+                     "{base::as.symbol(sdg_name)}" := 1,) %>%
+    dplyr::ungroup() %>%
+    dplyr::as_tibble() %>%
+    dplyr::arrange(doc_id) %>%
+    dplyr::distinct()
+  
+  sdg_match <- prior_posterior_match_tibble
+  # dplyr::bind_rows(sdg_match, prior_posterior_match_tibble)
+  
+  data_matched_result <-
+    dplyr::inner_join(dataIn,
+                      sdg_match,
+                      by = "doc_id" )
+  
+  return(data_matched_result)
 }
-
 
 # test <- function(dataIn = import_data(),
 #                  sdg = 1,
